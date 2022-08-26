c1af586d3219ecb6b0937f4d5e2056708869fe47
skrowll <lucasdejesus.rocha@hotmail.com>
2022-08-26 18:10:27 -0300

    feat(models): create user model

---
+++ src/database/models/user.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @param {import('sequelize').Sequelize} sequelize
+ * @param {import('sequelize').DataTypes} DataTypes
+*/ 
+
+const createUserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    tableName: 'Users',
+    timestamps: false,
+  });
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'userId',
+    })
+  }
+  return User;
+};
+
+module.exports = createUserModel;

96e15a38c2fb851f26869619b1b29e762172a316
skrowll <lucasdejesus.rocha@hotmail.com>
2022-08-26 18:22:19 -0300

    feat(models): create blogPost model

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @param {import('sequelize').Sequelize} sequelize
+ * @param {import('sequelize').DataTypes} DataTypes
+*/ 
+
+const createBlogPostModel = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  }, {
+    tableName: 'BlogPosts',
+    timestamps: false,
+  })
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'userId', as: 'user'
+    });
+  };
+  return BlogPost;
+};
+
+module.exports = createBlogPostModel;

c819b87b6cd3e7f154768855ff73ef6020303a78
skrowll <lucasdejesus.rocha@hotmail.com>
2022-08-29 17:29:49 -0300

    feat(app): add userRouter

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const loginRouter = require('./routers/login.router');
+const userRouter = require('./routers/user.router');
 
 const errorHandler = require('./middlewares/error.middleware');
 
@@ -13,6 +14,7 @@ app.use(express.json());
 // ...
 
 app.use('/login', loginRouter);
+app.use('/user', userRouter);
 
 app.use(errorHandler);
 

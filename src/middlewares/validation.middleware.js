c6346528fde2a6e78c89d663e5ae1e1192bca5d4
skrowll <lucasdejesus.rocha@hotmail.com>
2022-08-29 17:27:53 -0300

    feat(middlewares): create user validation middleware

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -5,6 +5,24 @@ const validations = {
         }
         return true;
     },
+    userValidate: (displayName, email, password) => {
+        const message = {
+            displayName: '"displayName" length must be at least 8 characters long',
+            email: '"email" must be a valid email',
+            password: '"password" length must be at least 6 characters long',
+            user: 'User already registered',
+        };
+        if (displayName.length < 8) {
+            return message.displayName;
+        }
+        const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        if (!emailRegex.test(email)) {
+            return message.email;
+        }
+        if (password.length < 6) {
+            return message.password;
+        }
+    },
 };
 
 module.exports = validations;

000a4075be67f2b50cd40d0f6ae98889856b09c0
skrowll <lucasdejesus.rocha@hotmail.com>
2022-08-29 16:46:48 -0300

    feat(app): add loginRouter and errorHandler on app

--- src/api.js
+++ src/api.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const loginRouter = require('./routers/login.router');
+
+const errorHandler = require('./middlewares/error.middleware');
+
 // ...
 
 const app = express();
@@ -7,6 +11,10 @@ const app = express();
 app.use(express.json());
 
 // ...
+
+app.use('/login', loginRouter);
+
+app.use(errorHandler);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`

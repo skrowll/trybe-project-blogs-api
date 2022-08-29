689ec8a47a5fe85ac62ac224d48f58bb77cafa71
skrowll <lucasdejesus.rocha@hotmail.com>
2022-08-29 17:58:35 -0300

    feat(app): add postRouter

--- src/api.js
+++ src/api.js
@@ -3,6 +3,7 @@ const express = require('express');
 const loginRouter = require('./routers/login.router');
 const userRouter = require('./routers/user.router');
 const categoryRouter = require('./routers/category.router');
+const postRouter = require('./routers/post.router');
 
 const errorHandler = require('./middlewares/error.middleware');
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use('/login', loginRouter);
 app.use('/user', userRouter);
 app.use('/categories', categoryRouter);
+app.use('/post', postRouter);
 
 app.use(errorHandler);
 

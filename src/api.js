12f57a3f6b2ae51acf05d227d0a4ad5225f04a22
skrowll <lucasdejesus.rocha@hotmail.com>
2022-08-29 17:45:54 -0300

    feat(app): add categoryRouter

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const loginRouter = require('./routers/login.router');
 const userRouter = require('./routers/user.router');
+const categoryRouter = require('./routers/category.router');
 
 const errorHandler = require('./middlewares/error.middleware');
 
@@ -15,6 +16,7 @@ app.use(express.json());
 
 app.use('/login', loginRouter);
 app.use('/user', userRouter);
+app.use('/categories', categoryRouter);
 
 app.use(errorHandler);
 
